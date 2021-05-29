cbe9e49a5bea05ffa2b894672f47ea8164cf8934
martucciv <vmartucci@knights.ucf.edu>
2021-05-29 14:57:26 -0400

    finished code

--- src/main/java/Printing.java
+++ src/main/java/Printing.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class Printing {
     public static void main(String[] args){
-        
+        System.out.print("What is the quote? ");
+        Scanner qInput = new Scanner(System.in);
+        String quote = qInput.nextLine();
+
+        System.out.print("Who said it? ");
+        Scanner aInput = new Scanner(System.in);
+        String author = aInput.nextLine();
+
+        System.out.printf("%s says, \"%s\"", author, quote);
     }
 }
